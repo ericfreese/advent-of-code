eecd7e041820b551e59bfcf052f2413d0cd214c1
Eric Freese <ericdfreese@gmail.com>
2024-12-06 12:45:19 -0700

    Clean up 2024 day 4

--- 2024/solutions/04-1.py
+++ 2024/solutions/04-1.py
@@ -1,16 +1,20 @@
-def cell_value(grid, row, col):
+with open("inputs/04", "r") as f:
+    grid = [list(line.strip()) for line in f]
+
+
+def cell_value(row, col):
     chars = grid[row] if row >= 0 and row < len(grid) else []
     return chars[col] if col >= 0 and col < len(chars) else None
 
 
-def search(grid, row, col, row_dir, col_dir):
+def search(row, col, row_dir, col_dir):
     return all(
-        cell_value(grid, row + (i * row_dir), col + (i * col_dir)) == char
+        cell_value(row + (i * row_dir), col + (i * col_dir)) == char
         for i, char in enumerate(list("XMAS"))
     )
 
 
-def check(grid, row, col):
+def check(row, col):
     directions = [
         (0, 1),
         (0, -1),
@@ -23,18 +27,11 @@ def check(grid, row, col):
     ]
 
     return sum(
-        1 if search(grid, row, col, row_dir, col_dir) else 0
+        1 if search(row, col, row_dir, col_dir) else 0
         for row_dir, col_dir in directions
     )
 
 
-with open("inputs/04", "r") as f:
-    grid = [list(line.strip()) for line in f]
-
-    print(
-        sum(
-            check(grid, row, col)
-            for row, chars in enumerate(grid)
-            for col in range(len(chars))
-        )
-    )
+print(
+    sum(check(row, col) for row, chars in enumerate(grid) for col in range(len(chars)))
+)
--- 2024/solutions/04-2.py
+++ 2024/solutions/04-2.py
@@ -1,16 +1,20 @@
-def cell_value(grid, row, col):
+with open("inputs/04", "r") as f:
+    grid = [list(line.strip()) for line in f]
+
+
+def cell_value(row, col):
     chars = grid[row] if row >= 0 and row < len(grid) else []
     return chars[col] if col >= 0 and col < len(chars) else None
 
 
-def search(grid, row, col, row_dir, col_dir):
+def search(row, col, row_dir, col_dir):
     return all(
-        cell_value(grid, row + (i * row_dir), col + (i * col_dir)) == char
+        cell_value(row + (i * row_dir), col + (i * col_dir)) == char
         for i, char in [(-1, "M"), (0, "A"), (1, "S")]
     )
 
 
-def check(grid, row, col):
+def check(row, col):
     directions = [
         (1, 1),
         (1, -1),
@@ -19,18 +23,15 @@ def check(grid, row, col):
     ]
 
     return sum(
-        1 if search(grid, row, col, row_dir, col_dir) else 0
+        1 if search(row, col, row_dir, col_dir) else 0
         for row_dir, col_dir in directions
     )
 
 
-with open("inputs/04", "r") as f:
-    grid = [list(line.strip()) for line in f]
-
-    print(
-        sum(
-            1 if check(grid, row, col) == 2 else 0
-            for row, chars in enumerate(grid)
-            for col in range(len(chars))
-        )
+print(
+    sum(
+        1 if check(row, col) == 2 else 0
+        for row, chars in enumerate(grid)
+        for col in range(len(chars))
     )
+)
